0999112281bff2ec5f9ab16467a73525162300f9
Sunaookami <Ungludo.pro@gmail.com>
2024-07-16 16:33:15 +0200

    Add files via upload
    ajout du main.py et du requirement.txt

---
+++ src/app.py
@@ -0,0 +1,50 @@
+import customtkinter
+
+def main():
+    app = App()
+    app.mainloop()
+
+# Interface principale
+
+customtkinter.set_appearance_mode("System")  # Modes: "System" (standard), "Dark", "Light"
+customtkinter.set_default_color_theme("dark-blue")  # Themes: "blue" (standard), "green", "dark-blue"
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+        # Configure Windows 
+        self.title("CookHub")
+        self.geometry(f"{1100}x{580}")
+
+        # Configure main frame
+        self.main_frame = customtkinter.CTkFrame(self)
+        self.main_frame.grid(row=0, column=1, rowspan=3)
+        self.main_frame.grid_columnconfigure(1, weight=1)
+        self.main_frame.grid_rowconfigure(1, weight=1)
+
+         # create sidebar frame with widgets
+        self.sidebar_frame = customtkinter.CTkFrame(self, width=140, corner_radius=0)
+        self.sidebar_frame.grid(row=0, column=0, rowspan=4, sticky="nsew")
+        self.sidebar_frame.grid_rowconfigure(5, weight=1)
+        self.logo_label = customtkinter.CTkLabel(self.sidebar_frame, text="CookHub", font=customtkinter.CTkFont(size=20, weight="bold"))
+        self.logo_label.grid(row=0, column=0, padx=20, pady=(20, 10))
+        # self.sidebar_button_1 = customtkinter.CTkButton(self.sidebar_frame, text= "",command=self.split_pdf)
+        # self.sidebar_button_1.grid(row=1, column=0, padx=20, pady=10)
+        # self.sidebar_button_2 = customtkinter.CTkButton(self.sidebar_frame, text="Envoyer", command=self.send_emails)
+        # self.sidebar_button_2.grid(row=2, column=0, padx=20, pady=10)
+        self.appearance_mode_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["Light", "Dark", "System"],
+                                                                       command=self.change_appearance_mode_event)
+        self.appearance_mode_optionemenu.grid(row=6, column=0, padx=20, pady=(10, 10))
+        self.scaling_optionemenu = customtkinter.CTkOptionMenu(self.sidebar_frame, values=["80%", "90%", "100%", "110%", "120%"],
+                                                               command=self.change_scaling_event)
+        self.scaling_optionemenu.grid(row=8, column=0, padx=20, pady=(10, 20))
+    
+    def change_appearance_mode_event(self, new_appearance_mode: str):
+        customtkinter.set_appearance_mode(new_appearance_mode)
+
+    def change_scaling_event(self, new_scaling: str):
+        new_scaling_float = int(new_scaling.replace("%", "")) / 100
+        customtkinter.set_widget_scaling(new_scaling_float)
+
+if __name__ == "__main__":
+    main()
